57913e17d93eb49560ccafbae7b635ff32c707e9
Diego <96022404+dzfrias@users.noreply.github.com>
2022-11-25 22:28:25 -0800

    feat(css): improve website look by a lot

--- src/editor/mod.rs
+++ src/editor/mod.rs
@@ -12,12 +12,17 @@ pub fn Editor() -> Html {
     };
 
     html! {
-        <div class={classes!("mx-4")}>
-            <ul class={classes!("py-1", "pl-4", "bg-teal-100", "pt-2", "rounded")}>
-                <RunButton {set_output}/>
-            </ul>
-            <textarea id="editor" rows=20 spellcheck="false" placeholder="Enter code here" style="resize: None" class={classes!("w-full", "font-mono", "text-ml", "p-2")}></textarea>
-            <p id="output" style="white-space: pre-line;" class={classes!("font-mono")}>{(*output).clone()}</p>
+        <div class={classes!("mx-4", "grid", "md:grid-cols-2", "gap-2", "h-2/3")}>
+            <div class={classes!("h-full")}>
+                <ul class={classes!("py-1", "pl-4", "bg-yellow-900", "pt-2", "rounded")}>
+                    <RunButton {set_output}/>
+                </ul>
+                <textarea id="editor" spellcheck="false" placeholder="Enter code here" style="resize: None" class={classes!("w-full", "font-mono", "text-ml", "p-2", "h-full", "focus:outline-none")}></textarea>
+            </div>
+            <div class={classes!("h-full")}>
+                <p class={classes!("py-1", "pl-4", "bg-yellow-900", "pt-2", "rounded", "text-2xl", "text-center", "text-yellow-100")}>{"Output"}</p>
+                <p id="output" class={classes!("font-mono", "bg-white", "w-full", "h-full", "p-2", "text-ml", "whitespace-pre")}>{(*output).clone()}</p>
+            </div>
         </div>
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,8 @@ use yew::prelude::*;
 #[function_component]
 fn App() -> Html {
     html! {
-        <div>
-            <h1 class={classes!("bg-sky-300", "text-4xl", "py-2", "pl-2", "italic", "mb-5")}>{"Scurry"}</h1>
+        <div class={classes!("h-screen")}>
+            <h1 class={classes!("bg-orange-900", "text-4xl", "py-3", "pl-2", "italic", "mb-5", "text-yellow-400")}>{"Scurry"}</h1>
             <Editor/>
         </div>
     }

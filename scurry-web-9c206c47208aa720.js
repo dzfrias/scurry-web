1a6f970d3ea2fe788d140c336c8c94a0dee038f8
dzfrias <dzfrias@users.noreply.github.com>
2022-11-27 05:07:40 +0000

    deploy: 4eb9bae12b28f21a2f86ca96b700e72589bdd1f6

--- scurry-web-9c206c47208aa720.js
+++ scurry-web-9c206c47208aa720.js
@@ -643,12 +643,12 @@ function getImports() {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper422 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 146, __wbg_adapter_16);
+    imports.wbg.__wbindgen_closure_wrapper421 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 145, __wbg_adapter_16);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper784 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 272, __wbg_adapter_19);
+    imports.wbg.__wbindgen_closure_wrapper783 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 271, __wbg_adapter_19);
         return addHeapObject(ret);
     };
 
@@ -686,7 +686,7 @@ function initSync(module) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('scurry-web-a16a9e5e02cc3936_bg.wasm', import.meta.url);
+        input = new URL('scurry-web-9c206c47208aa720_bg.wasm', import.meta.url);
     }
     const imports = getImports();
 

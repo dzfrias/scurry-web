670b2467a051f1f97f4d4b93cd309f34e7754c80
Diego <96022404+dzfrias@users.noreply.github.com>
2022-11-26 18:33:02 -0800

    feat: remove description on small devices

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ fn App() -> Html {
             <div class={classes!("bg-orange-900", "mb-5")}>
                 <div class={classes!("flex")}>
                     <h1 class={classes!("text-4xl", "py-3", "pl-2", "italic", "text-yellow-400", "flex-1")}>
-                    {"Scurry"}<span class={classes!("text-xs", "text-yellow-500", "pl-1")}>{"A new take on object-oriented languages"}</span>
+                    {"Scurry"}<span class={classes!("text-xs", "text-yellow-500", "pl-1", "hidden", "sm:inline-flex")}>{"A new take on object-oriented languages"}</span>
                     </h1>
                     <ul class={classes!("pr-9", "self-center", "flex", "gap-10", "text-amber-500", "text-lg")}>
                         // TODO: Link here
